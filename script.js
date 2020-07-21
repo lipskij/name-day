6c98df49e0a952a7ccba5c23e7ff6a3350ea52aa
lipskij <emil.lipskij@gmail.com>
2020-07-21 19:24:59 +0300

    search for name async await

--- script.js
+++ script.js
@@ -1,22 +1,50 @@
 const nameBtn = document.querySelector("#name-btn");
-const apiData = {
-  url: "https://api.abalin.net/today?",
-  country: "country=lt",
-};
+const searchBar = document.querySelector("#searchbar");
+const searchBtn = document.querySelector("#search-btn");
+const searchNameApi = "https://api.abalin.net/getdate?name=&country=lt";
 
 nameBtn.addEventListener("click", function (e) {
   e.preventDefault();
 
-  const {url, country } = apiData;
-  const apiUrl = `${url}${country}`;
-
-  fetch(apiUrl)
-    .then((data) => data.json())
-    .then((names) => generateHtml(names));
+  const loadNames = async () => {
+    try {
+      const res = await fetch("https://api.abalin.net/today?country=lt");
+      let names = await res.json();
+      generateHtml(names);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   const generateHtml = (data) => {
     const html = `<p class="name">${data.data.namedays.lt}</p>`;
-    const nameSpan = document.querySelector('#names');
+    const nameSpan = document.querySelector("#names");
     nameSpan.innerHTML = html;
-  }
+  };
+  loadNames();
+});
+
+searchBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  const loadDates = async () => {
+    try {
+      const res = await fetch(
+        `https://api.abalin.net/getdate?name=${searchBar.value}&country=lt`
+      );
+      let dates = await res.json();
+      generateDates(dates);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const generateDates = (data) => {
+    const dateTxt = `<p>${data.results}</p>`;
+    dateTxt.filter((date) => {
+      return date.day.contains(searchBar.value);
+    });
+  }; // iterate through items in the array of objects.
+
+  loadDates();
 });
